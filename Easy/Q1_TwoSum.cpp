f80f85bafc18827c9fc30941cbb8df794c724493
mchangchien <67855568+mchangchien@users.noreply.github.com>
2021-10-23 12:33:39 +0800

    improve Q1 ans

--- Easy/Q1_TwoSum.cpp
+++ Easy/Q1_TwoSum.cpp
@@ -4,20 +4,13 @@ public:
       vector<int> ans(2,0);  
       for (int i = 0;i<nums.size();i++)
       {
-        for (int j = 0;j<nums.size();j++)
+        for (int j = i+1;j<nums.size();j++)
         {
-          if (i==j)
+          if((nums[i]+nums[j])==target)
           {
-            // do nothing
-          }
-          else
-          {
-            if((nums[i]+nums[j])==target)
-            {
-              ans[0]=i;
-              ans[1]=j;
-              return ans;
-            }
+            ans[0]=i;
+            ans[1]=j;
+            return ans;
           }
         }
       }
